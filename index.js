7a081f75874d0e79996e522d48691f8fcfae6f40
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-03-17 15:46:29 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array){
+    const find = array.find(element => element.result === "W");
+    if(find === undefined){
+      return undefined;
+    }else{
+      return find.year;
+    };
+  };
